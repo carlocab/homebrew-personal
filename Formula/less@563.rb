ac55ac092d8c87e419654177196c9e86f838b2fb
carlocab <30379873+carlocab@users.noreply.github.com>
2020-11-06 02:54:43 +0000

    Update `less` formula
    I hope this works!

--- Formula/less@563.rb
+++ Formula/less@563.rb
@@ -1,7 +1,7 @@
-class LessAT551 < Formula
+class LessAT563 < Formula
   desc "Pager program similar to more"
   homepage "http://www.greenwoodsoftware.com/less/index.html"
-  url "http://www.greenwoodsoftware.com/less/less-551.tar.gz"
+  url "http://www.greenwoodsoftware.com/less/less-563.tar.gz"
   sha256 "ff165275859381a63f19135a8f1f6c5a194d53ec3187f94121ecd8ef0795fe3d"
   license "GPL-3.0-or-later"
 
@@ -11,10 +11,11 @@ class LessAT551 < Formula
   end
 
   depends_on "pcre"
-
-  uses_from_macos "ncurses"
+  depends_on "ncurses"
+  depends_on "llvm" => :build
 
   def install
+    ENV.append "CC", "/usr/local/opt/llvm/bin/clang"
     system "./configure", "--prefix=#{prefix}", "--with-regex=pcre"
     system "make", "install"
   end
