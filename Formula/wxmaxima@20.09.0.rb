2c7472904abed8d4408fe7882750094f8128ce65
carlocab <30379873+carlocab@users.noreply.github.com>
2020-12-07 11:19:46 +0000

    wxmaxima@20.09.0 (new formula)
    Closes #12.
    
    Signed-off-by: Carlo Cabrera <30379873+carlocab@users.noreply.github.com>

---
+++ Formula/wxmaxima@20.09.0.rb
@@ -0,0 +1,48 @@
+class WxmaximaAT20090 < Formula
+  desc "Cross platform GUI for Maxima"
+  homepage "https://wxmaxima-developers.github.io/wxmaxima/"
+  url "https://github.com/wxMaxima-developers/wxmaxima/archive/Version-20.09.0.tar.gz"
+  sha256 "a2ba6797642c7efa96c5dbb6249134a0ace246ebd390e42f7c227fa94609ef27"
+  license "GPL-2.0-or-later"
+  head "https://github.com/wxMaxima-developers/wxmaxima.git"
+
+  keg_only :versioned_formula
+
+  depends_on "cmake" => :build
+  depends_on "gettext" => :build
+  depends_on "ninja" => :build
+  depends_on "maxima"
+  depends_on "wxmac"
+
+  def install
+    # en_US.UTF8 is not a valid locale for macOS
+    # https://github.com/wxMaxima-developers/wxmaxima/issues/1402
+    inreplace "src/StreamUtils.cpp", "en_US.UTF8", "en_US.UTF-8"
+
+    mkdir "build-wxm" do
+      system "cmake", "..", "-GNinja", *std_cmake_args
+      system "ninja"
+      system "ninja", "install"
+      prefix.install "src/wxMaxima.app"
+    end
+
+    bash_completion.install "data/wxmaxima"
+    bin.write_exec_script "#{prefix}/wxMaxima.app/Contents/MacOS/wxmaxima"
+  end
+
+  def caveats
+    <<~EOS
+      When you start wxMaxima the first time, set the path to Maxima
+      (e.g. #{HOMEBREW_PREFIX}/bin/maxima) in the Preferences.
+
+      Enable gnuplot functionality by setting the following variables
+      in ~/.maxima/maxima-init.mac:
+        gnuplot_command:"#{HOMEBREW_PREFIX}/bin/gnuplot"$
+        draw_command:"#{HOMEBREW_PREFIX}/bin/gnuplot"$
+    EOS
+  end
+
+  test do
+    assert_match "algebra", shell_output("#{bin}/wxmaxima --help 2>&1")
+  end
+end
