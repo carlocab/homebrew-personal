121b36668770fa0e9dc3c4aa0528eac44bdbbf67
carlocab <30379873+carlocab@users.noreply.github.com>
2020-12-10 09:43:01 +0000

    unrar 6.0.3 (new formula)
    Closes #13.
    
    Signed-off-by: Carlo Cabrera <30379873+carlocab@users.noreply.github.com>

---
+++ Formula/unrar.rb
@@ -0,0 +1,51 @@
+class Unrar < Formula
+  desc "Extract, view, and test RAR archives"
+  homepage "https://www.rarlab.com/"
+  url "https://www.rarlab.com/rar/unrarsrc-6.0.3.tar.gz"
+  sha256 "1def53392d879f9e304aa6eac1339cf41f9bce1111a2f5845071665738c4aca0"
+  license :cannot_represent
+
+  livecheck do
+    url "https://www.rarlab.com/rar_add.htm"
+    regex(/href=.*?unrarsrc[._-]v?(\d+(?:\.\d+)+)\.t/i)
+  end
+
+  def install
+    # upstream doesn't particularly care about their unix targets,
+    # so we do the dirty work of renaming their shared objects to
+    # dylibs for them.
+    inreplace "makefile", "libunrar.so", "libunrar.dylib"
+
+    system "make"
+    bin.install "unrar"
+
+    # Explicitly clean up for the library build to avoid an issue with an
+    # apparent implicit clean which confuses the dependencies.
+    system "make", "clean"
+    system "make", "lib"
+    lib.install shared_library("libunrar")
+
+    prefix.install "license.txt"
+  end
+
+  def caveats
+    <<~EOS
+      The license has been installed at #{opt_prefix}/license.txt. Please ensure that you
+      agree with the terms of the license before using this software.
+    EOS
+  end
+
+  test do
+    contentpath = "directory/file.txt"
+    rarpath = testpath/"archive.rar"
+    data =  "UmFyIRoHAM+QcwAADQAAAAAAAACaCHQggDIACQAAAAkAAAADtPej1LZwZE" \
+            "QUMBIApIEAAGRpcmVjdG9yeVxmaWxlLnR4dEhvbWVicmV3CsQ9ewBABwA="
+
+    rarpath.write data.unpack1("m")
+    assert_equal contentpath, `#{bin}/unrar lb #{rarpath}`.strip
+    assert_equal 0, $CHILD_STATUS.exitstatus
+
+    system "#{bin}/unrar", "x", rarpath, testpath
+    assert_equal "Homebrew\n", (testpath/contentpath).read
+  end
+end
