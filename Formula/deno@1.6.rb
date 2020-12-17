b15c521fa20f2209cd079053cc2f97c3bf9a9285
carlocab <30379873+carlocab@users.noreply.github.com>
2020-12-17 14:39:52 +0000

    deno@1.6 1.6.1 (new formula)

---
+++ Formula/deno@1.6.rb
@@ -0,0 +1,46 @@
+class DenoAT16 < Formula
+  desc "Secure runtime for JavaScript and TypeScript"
+  homepage "https://deno.land/"
+  url "https://github.com/denoland/deno/releases/download/v1.6.1/deno_src.tar.gz"
+  sha256 "8ea92caf87c18d19ee5b1acf4dc887206cde9153ccc53dddf534f6c30cf3e6c5"
+  license "MIT"
+
+  depends_on "llvm" => :build
+  depends_on "rust" => :build
+  depends_on xcode: ["10.0", :build] # required by v8 7.9+
+  depends_on :macos # Due to Python 2 (see https://github.com/denoland/deno/issues/2893)
+
+  uses_from_macos "xz"
+
+  def install
+    # env args for building a release build with our clang, ninja and gn
+    ENV["GN"] = buildpath/"gn/out/gn"
+    # build rusty_v8 from source
+    ENV["V8_FROM_SOURCE"] = "1"
+    # overwrite Chromium minimum sdk version of 10.15
+    ENV["FORCE_MAC_SDK_MIN"] = "10.13"
+    # build with llvm and link against system libc++ (no runtime dep)
+    ENV["CLANG_BASE_PATH"] = Formula["llvm"].prefix
+    ENV.remove "HOMEBREW_LIBRARY_PATHS", Formula["llvm"].opt_lib
+
+    cd "cli" do
+      system "cargo", "install", "-vv", *std_cargo_args
+    end
+
+    # Install bash and zsh completion
+    output = Utils.safe_popen_read("#{bin}/deno", "completions", "bash")
+    (bash_completion/"deno").write output
+    output = Utils.safe_popen_read("#{bin}/deno", "completions", "zsh")
+    (zsh_completion/"_deno").write output
+  end
+
+  test do
+    (testpath/"hello.ts").write <<~EOS
+      console.log("hello", "deno");
+    EOS
+    assert_match "hello deno", shell_output("#{bin}/deno run hello.ts")
+    assert_match "console.log",
+      shell_output("#{bin}/deno run --allow-read=#{testpath} https://deno.land/std@0.50.0/examples/cat.ts " \
+                   "#{testpath}/hello.ts")
+  end
+end
