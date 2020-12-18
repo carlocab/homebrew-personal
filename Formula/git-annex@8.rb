21a313e172b60e4acdc141dee61b5fd37be92a81
carlocab <30379873+carlocab@users.noreply.github.com>
2020-12-18 18:36:54 +0000

    git-annex@8 8.20201127 (new formula)

---
+++ Formula/git-annex@8.rb
@@ -0,0 +1,80 @@
+class GitAnnexAT8 < Formula
+  desc "Manage files with git without checking in file contents"
+  homepage "https://git-annex.branchable.com/"
+  url "https://hackage.haskell.org/package/git-annex-8.20201103/git-annex-8.20201103.tar.gz"
+  sha256 "f4a6ade8a86d9640b5b2d81e45fc4c93c8e35bd90a1fc49d47c4aff1f59d31fd"
+  license all_of: ["AGPL-3.0-or-later", "BSD-2-Clause", "BSD-3-Clause",
+                   "GPL-2.0-only", "GPL-3.0-or-later", "MIT"]
+  head "git://git-annex.branchable.com/"
+
+  livecheck do
+    url :stable
+  end
+
+  depends_on "ghc@8.8" => :build
+  depends_on "haskell-stack" => :build
+  depends_on "pkg-config" => :build
+  depends_on "gsasl"
+  depends_on "libmagic"
+  depends_on "quvi"
+
+  def install
+    system "stack", "build", "--system-ghc", "--no-install-ghc"
+    system "make", "install", "BUILDER=stack", "PREFIX=#{prefix}"
+    bin.install_symlink "git-annex" => "git-annex-shell"
+  end
+
+  plist_options manual: "git annex assistant --autostart"
+
+  def plist
+    <<~EOS
+      <?xml version="1.0" encoding="UTF-8"?>
+      <!DOCTYPE plist PUBLIC "-//Apple//DTD PLIST 1.0//EN" "http://www.apple.com/DTDs/PropertyList-1.0.dtd">
+      <plist version="1.0">
+        <dict>
+          <key>Label</key>
+          <string>#{plist_name}</string>
+          <key>RunAtLoad</key>
+          <true/>
+          <key>KeepAlive</key>
+          <false/>
+          <key>ProgramArguments</key>
+          <array>
+            <string>#{opt_bin}/git-annex</string>
+            <string>assistant</string>
+            <string>--autostart</string>
+          </array>
+        </dict>
+      </plist>
+    EOS
+  end
+
+  test do
+    # make sure git can find git-annex
+    ENV.prepend_path "PATH", bin
+    # We don't want this here or it gets "caught" by git-annex.
+    rm_r "Library/Python/2.7/lib/python/site-packages/homebrew.pth"
+
+    system "git", "init"
+    system "git", "annex", "init"
+    (testpath/"Hello.txt").write "Hello!"
+    assert !File.symlink?("Hello.txt")
+    assert_match /^add Hello.txt.*ok.*\(recording state in git\.\.\.\)/m, shell_output("git annex add .")
+    system "git", "commit", "-a", "-m", "Initial Commit"
+    assert File.symlink?("Hello.txt")
+
+    # make sure the various remotes were built
+    assert_match shell_output("git annex version | grep 'remote types:'").chomp,
+                 "remote types: git gcrypt p2p S3 bup directory rsync web bittorrent " \
+                 "webdav adb tahoe glacier ddar git-lfs httpalso hook external"
+
+    # The steps below are necessary to ensure the directory cleanly deletes.
+    # git-annex guards files in a way that isn't entirely friendly of automatically
+    # wiping temporary directories in the way `brew test` does at end of execution.
+    system "git", "rm", "Hello.txt", "-f"
+    system "git", "commit", "-a", "-m", "Farewell!"
+    system "git", "annex", "unused"
+    assert_match "dropunused 1 ok", shell_output("git annex dropunused 1 --force")
+    system "git", "annex", "uninit"
+  end
+end
