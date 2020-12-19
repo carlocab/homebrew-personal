17ca9540aea50eb576247e6fafa7003407d3962c
carlocab <30379873+carlocab@users.noreply.github.com>
2020-12-19 09:45:09 +0000

    allureofthestars@0.9 0.9.5.0 (new formula)

---
+++ Formula/allureofthestars@0.9.rb
@@ -0,0 +1,32 @@
+class AllureofthestarsAT09 < Formula
+  desc "Near-future Sci-Fi roguelike and tactical squad combat game"
+  homepage "https://www.allureofthestars.com/"
+  url "https://hackage.haskell.org/package/Allure-0.9.5.0/Allure-0.9.5.0.tar.gz"
+  sha256 "8180fe070633bfa5515de8f7443421044e7ad4ee050f0a92c048cec5f2c88132"
+  license "AGPL-3.0"
+  head "https://github.com/AllureOfTheStars/Allure.git"
+
+  livecheck do
+    url :stable
+  end
+
+  depends_on "cabal-install" => :build
+  depends_on "ghc@8.8" => :build
+  depends_on "pkg-config" => :build
+  depends_on "gmp"
+  depends_on "sdl2_ttf"
+
+  def install
+    system "cabal", "v2-update"
+    system "cabal", "v2-install", *std_cabal_v2_args
+  end
+
+  test do
+    assert_equal "",
+      shell_output("#{bin}/Allure --dbgMsgSer --dbgMsgCli --logPriority 0 --newGame 3 --maxFps 100000 " \
+                                 "--stopAfterFrames 50 --automateAll --keepAutomated --gameMode battle " \
+                                 "--setDungeonRng 7 --setMainRng 7")
+    assert_equal "", shell_output("cat ~/.Allure/stderr.txt")
+    assert_match "UI client FactionId 1 stopped", shell_output("cat ~/.Allure/stdout.txt")
+  end
+end
