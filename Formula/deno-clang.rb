6fb43a596f95490090a1c132f2b57727c32085e2
carlocab <30379873+carlocab@users.noreply.github.com>
2021-01-01 03:49:43 +0000

    deno-clang 1.6.3 (new formula)

---
+++ Formula/deno-clang.rb
@@ -0,0 +1,48 @@
+class DenoClang < Formula
+  desc "Secure runtime for JavaScript and TypeScript"
+  homepage "https://deno.land/"
+  url "https://github.com/denoland/deno/releases/download/v1.6.3/deno_src.tar.gz"
+  sha256 "457b5dc1fd40b7f9368b7c26c300f19f80caace4e3a19340b7ea037d6383b221"
+  license "MIT"
+
+  depends_on "ninja" => :build
+  depends_on "rust" => :build
+  depends_on :macos # Due to Python 2 (see https://github.com/denoland/deno/issues/2893)
+
+  uses_from_macos "xz"
+
+  def install
+    ENV["RUST_BACKTRACE"] = "1"
+    # env args for building a release build with our clang, ninja and gn
+    ENV["GN"] = buildpath/"gn/out/gn"
+    # build rusty_v8 from source
+    ENV["V8_FROM_SOURCE"] = "1"
+    # build with system clang
+    ENV["CLANG_BASE_PATH"] = "/usr"
+    # build with Homebrew ninja
+    ENV["NINJA"] = Formula["ninja"].opt_bin/"ninja"
+    # overwrite Chromium minimum sdk version of 10.15
+    ENV["FORCE_MAC_SDK_MIN"] = MacOS.version
+
+    cd "cli" do
+      system "cargo", "install", "-vv", *std_cargo_args
+    end
+
+    bash_output = Utils.safe_popen_read("#{bin}/deno", "completions", "bash")
+    (bash_completion/"deno").write bash_output
+    zsh_output = Utils.safe_popen_read("#{bin}/deno", "completions", "zsh")
+    (zsh_completion/"_deno").write zsh_output
+    fish_output = Utils.safe_popen_read("#{bin}/deno", "completions", "fish")
+    (fish_completion/"deno.fish").write fish_output
+  end
+
+  test do
+    (testpath/"hello.ts").write <<~EOS
+      console.log("hello", "deno");
+    EOS
+    assert_match "hello deno", shell_output("#{bin}/deno run hello.ts")
+    assert_match "console.log",
+      shell_output("#{bin}/deno run --allow-read=#{testpath} https://deno.land/std@0.50.0/examples/cat.ts " \
+                   "#{testpath}/hello.ts")
+  end
+end
