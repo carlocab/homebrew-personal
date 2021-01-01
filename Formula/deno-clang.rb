3de03158b90e2d634a5de7222c469734b155072f
carlocab <30379873+carlocab@users.noreply.github.com>
2021-01-01 04:15:04 +0000

    Build with LLVM

--- Formula/deno-clang.rb
+++ Formula/deno-clang.rb
@@ -5,6 +5,7 @@ class DenoClang < Formula
   sha256 "457b5dc1fd40b7f9368b7c26c300f19f80caace4e3a19340b7ea037d6383b221"
   license "MIT"
 
+  depends_on "llvm" => :build
   depends_on "ninja" => :build
   depends_on "rust" => :build
   depends_on :macos # Due to Python 2 (see https://github.com/denoland/deno/issues/2893)
@@ -12,17 +13,16 @@ class DenoClang < Formula
   uses_from_macos "xz"
 
   def install
-    ENV["RUST_BACKTRACE"] = "1"
     # env args for building a release build with our clang, ninja and gn
     ENV["GN"] = buildpath/"gn/out/gn"
+    ENV["NINJA"] = Formula["ninja"].opt_bin/"ninja"
     # build rusty_v8 from source
     ENV["V8_FROM_SOURCE"] = "1"
-    # build with system clang
-    ENV["CLANG_BASE_PATH"] = "/usr"
-    # build with Homebrew ninja
-    ENV["NINJA"] = Formula["ninja"].opt_bin/"ninja"
     # overwrite Chromium minimum sdk version of 10.15
     ENV["FORCE_MAC_SDK_MIN"] = MacOS.version
+    # build with llvm and link against system libc++ (no runtime dep)
+    ENV["CLANG_BASE_PATH"] = Formula["llvm"].prefix
+    ENV.remove "HOMEBREW_LIBRARY_PATHS", Formula["llvm"].opt_lib
 
     cd "cli" do
       system "cargo", "install", "-vv", *std_cargo_args
