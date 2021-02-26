52a9b6f5c5a0ec3b9d69517d51ffec38261fda20
Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
2021-02-26 17:00:23 +0000

    pulp@15.0.0 15.0.0 (new formula)

---
+++ Formula/pulp@15.0.0.rb
@@ -0,0 +1,26 @@
+require "language/node"
+
+class PulpAT1500 < Formula
+  desc "Build tool for PureScript projects"
+  homepage "https://github.com/bodil/pulp"
+  url "https://registry.npmjs.org/pulp/-/pulp-15.0.0.tgz"
+  sha256 "695da1581389d060810ed1a5962ab7e53696db8493b224fa7dc2358f255b8b53"
+  license "LGPL-3.0"
+
+  depends_on "bower"
+  depends_on "node"
+  depends_on "purescript"
+
+  def install
+    system "npm", "install", *Language::Node.std_npm_install_args(libexec)
+    bin.install_symlink Dir["#{libexec}/bin/*"]
+  end
+
+  test do
+    assert_match version.to_s, shell_output("#{bin}/pulp --version")
+
+    system("#{bin}/pulp", "init")
+    assert_predicate testpath/".gitignore", :exist?
+    assert_predicate testpath/"bower.json", :exist?
+  end
+end
