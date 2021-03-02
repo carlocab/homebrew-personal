ea9c71d880dce0d25a90b319dcb9bbdffde8eb7b
Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
2021-03-02 20:36:24 +0000

    gllvm: add 1.3.0 bottle.

--- Formula/gllvm.rb
+++ Formula/gllvm.rb
@@ -5,6 +5,13 @@ class Gllvm < Formula
   sha256 "e8bcb9b93bc2d24283fe45f16891ec5d70116b5fc49144a9c98832ed074f8782"
   license "BSD-3-Clause"
 
+  bottle do
+    root_url "https://github.com/carlocab/homebrew-personal/releases/download/gllvm-1.3.0"
+    sha256 cellar: :any_skip_relocation, big_sur:      "b5e321bef35dde914ae7cf847cba8339192e9df2468c76e59acb718b5d4b0c5a"
+    sha256 cellar: :any_skip_relocation, catalina:     "92444d800a22709396bcf80c8c6e883f539f2bd9ea629170cdbb777f383b0d56"
+    sha256 cellar: :any_skip_relocation, x86_64_linux: "d045c6275487fbfe9275f3f8a38fb8f1f51da6ee64f5adc020781e9af6ada9d3"
+  end
+
   depends_on "go" => :build
   depends_on "llvm"
 
