9551a7852878c848c77876463377ff9d602f589d
Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
2021-03-07 18:01:32 +0000

    ldns@1.7.1 1.7.1 (new formula)

---
+++ Formula/ldns@1.7.1.rb
@@ -0,0 +1,73 @@
+class LdnsAT171 < Formula
+  desc "DNS library written in C"
+  homepage "https://nlnetlabs.nl/projects/ldns/"
+  url "https://nlnetlabs.nl/downloads/ldns/ldns-1.7.1.tar.gz"
+  sha256 "8ac84c16bdca60e710eea75782356f3ac3b55680d40e1530d7cea474ac208229"
+  license "BSD-3-Clause"
+  revision 3
+
+  # https://nlnetlabs.nl/downloads/ldns/ since the first-party site has a
+  # tendency to lead to an `execution expired` error.
+  livecheck do
+    url "https://github.com/NLnetLabs/ldns.git"
+    regex(/^(?:release-)?v?(\d+(?:\.\d+)+)$/i)
+  end
+
+  depends_on "swig" => :build
+  depends_on "openssl@1.1"
+  depends_on "python@3.9"
+
+  def install
+    args = %W[
+      --prefix=#{prefix}
+      --with-drill
+      --with-examples
+      --with-ssl=#{Formula["openssl@1.1"].opt_prefix}
+      --with-pyldns
+      PYTHON_SITE_PKG=#{lib}/python3.9/site-packages
+      --disable-dane-verify
+    ]
+
+    # Fixes: ./contrib/python/ldns_wrapper.c:2746:10: fatal error: 'ldns.h' file not found
+    inreplace "contrib/python/ldns.i", "#include \"ldns.h\"", "#include <ldns/ldns.h>"
+
+    ENV["PYTHON"] = Formula["python@3.9"].opt_bin/"python3"
+    system "./configure", *args
+
+    inreplace "Makefile" do |s|
+      s.change_make_var! "PYTHON_LDFLAGS", "-undefined dynamic_lookup"
+      s.gsub!(/(\$\(PYTHON_LDFLAGS\).*) -no-undefined/, "\\1")
+    end
+
+    system "make"
+    system "make", "install"
+    system "make", "install-pyldns"
+    (lib/"pkgconfig").install "packaging/libldns.pc"
+  end
+
+  test do
+    l1 = <<~EOS
+      AwEAAbQOlJUPNWM8DQown5y/wFgDVt7jskfEQcd4pbLV/1osuBfBNDZX
+      qnLI+iLb3OMLQTizjdscdHPoW98wk5931pJkyf2qMDRjRB4c5d81sfoZ
+      Od6D7Rrx
+    EOS
+    l2 = <<~EOS
+      AwEAAb/+pXOZWYQ8mv9WM5dFva8WU9jcIUdDuEjldbyfnkQ/xlrJC5zA
+      EfhYhrea3SmIPmMTDimLqbh3/4SMTNPTUF+9+U1vpNfIRTFadqsmuU9F
+      ddz3JqCcYwEpWbReg6DJOeyu+9oBoIQkPxFyLtIXEPGlQzrynKubn04C
+      x83I6NfzDTraJT3jLHKeW5PVc1ifqKzHz5TXdHHTA7NkJAa0sPcZCoNE
+      1LpnJI/wcUpRUiuQhoLFeT1E432GuPuZ7y+agElGj0NnBxEgnHrhrnZW
+      UbULpRa/il+Cr5Taj988HqX9Xdm6FjcP4Lbuds/44U7U8du224Q8jTrZ
+      57Yvj4VDQKc=
+    EOS
+    (testpath/"powerdns.com.dnskey").write <<~EOS
+      powerdns.com.   10773 IN  DNSKEY  256 3 8  #{l1.tr!("\n", " ")}
+      powerdns.com.   10773 IN  DNSKEY  257 3 8  #{l2.tr!("\n", " ")}
+    EOS
+
+    system "#{bin}/ldns-key2ds", "powerdns.com.dnskey"
+
+    match = "d4c3d5552b8679faeebc317e5f048b614b2e5f607dc57f1553182d49ab2179f7"
+    assert_match match, File.read("Kpowerdns.com.+008+44030.ds")
+  end
+end
