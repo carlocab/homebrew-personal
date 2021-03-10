9fc644b212ff999aefd17bea6880c4b7e51ed8b5
Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
2021-03-10 04:53:20 +0000

    wllvm: add 1.3.0 bottle.

--- Formula/wllvm.rb
+++ Formula/wllvm.rb
@@ -7,6 +7,13 @@ class Wllvm < Formula
   sha256 "a98dd48350d8aae80fe03b92efb11c3e1b92f6aee482f4331f7c97265ca7a602"
   license "MIT"
 
+  bottle do
+    root_url "https://github.com/carlocab/homebrew-personal/releases/download/wllvm-1.3.0"
+    sha256 cellar: :any_skip_relocation, big_sur:      "3c68cdb4bf1a0c9e5e0856b94448b269c7c79fdd6213780dee4a2cf357791c78"
+    sha256 cellar: :any_skip_relocation, catalina:     "92f87f71ed8184bed9f344f0bab042da31088f12bcc971e8e4c948cfb2dea384"
+    sha256 cellar: :any_skip_relocation, x86_64_linux: "9ba7fb948e1bde159d6ceec28e5420e0cbbf38adf70c9790df60dbeb1af5f411"
+  end
+
   depends_on "llvm"
   depends_on "python@3.9"
 
