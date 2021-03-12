761f3ff7868b8f1c12153c92f4de2af57349c596
Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
2021-03-12 10:49:31 +0000

    minisat: add 2.2.1 bottle.

--- Formula/minisat.rb
+++ Formula/minisat.rb
@@ -6,6 +6,13 @@ class Minisat < Formula
   license "MIT"
   head "https://github.com/stp/minisat.git"
 
+  bottle do
+    root_url "https://github.com/carlocab/homebrew-personal/releases/download/minisat-2.2.1"
+    sha256 cellar: :any,                 big_sur:      "4c0d3e442b4d6b0c09788a16a700e6b9a72fd4a4fe6b6679e23406c1a988f0b0"
+    sha256 cellar: :any,                 catalina:     "c2f2dfd1254dd99debacea9c92c46305613bcdcd7cd71069483a2bb24fc81c76"
+    sha256 cellar: :any_skip_relocation, x86_64_linux: "2ebcc1b11de138365ac66d3f765ba7ccf23d847047b7eead7b0ebb89565fb4b1"
+  end
+
   depends_on "cmake" => :build
 
   uses_from_macos "zlib"
