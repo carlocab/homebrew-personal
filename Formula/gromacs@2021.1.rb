cefcef2093d9e24a5990916d859fde2e2574a976
Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
2021-05-08 17:37:05 +0100

    gromacs: test build

---
+++ Formula/gromacs@2021.1.rb
@@ -0,0 +1,57 @@
+class GromacsAT20211 < Formula
+  desc "Versatile package for molecular dynamics calculations"
+  homepage "https://www.gromacs.org/"
+  url "https://ftp.gromacs.org/pub/gromacs/gromacs-2021.1.tar.gz"
+  sha256 "bc1d0a75c134e1fb003202262fe10d3d32c59bbb40d714bc3e5015c71effe1e5"
+  license "LGPL-2.1-or-later"
+
+  livecheck do
+    url "https://ftp.gromacs.org/pub/gromacs/"
+    regex(/href=.*?gromacs[._-]v?(\d+(?:\.\d+)*)\.t/i)
+  end
+
+  depends_on "cmake" => :build
+  depends_on "fftw"
+  depends_on "gcc" # for OpenMP
+  depends_on "openblas"
+
+  fails_with gcc: "5"
+  fails_with gcc: "6"
+
+  def install
+    # Non-executable GMXRC files should be installed in DATADIR
+    inreplace "scripts/CMakeLists.txt", "CMAKE_INSTALL_BINDIR",
+                                        "CMAKE_INSTALL_DATADIR"
+
+    # Avoid superenv shim reference
+    inreplace "src/gromacs/gromacs-toolchain.cmake.cmakein", "@CMAKE_LINKER@",
+                                                             "/usr/bin/ld"
+
+    gcc_major_ver = Formula["gcc"].any_installed_version.major
+    args = std_cmake_args + %W[
+      -DCMAKE_C_COMPILER=gcc-#{gcc_major_ver}
+      -DCMAKE_CXX_COMPILER=g++-#{gcc_major_ver}
+    ]
+
+    mkdir "build" do
+      system "cmake", "..", *args
+      system "make", "install"
+    end
+
+    bash_completion.install "build/scripts/GMXRC" => "gromacs-completion.bash"
+    bash_completion.install "#{bin}/gmx-completion-gmx.bash" => "gmx-completion-gmx.bash"
+    bash_completion.install "#{bin}/gmx-completion.bash" => "gmx-completion.bash"
+    zsh_completion.install "build/scripts/GMXRC.zsh" => "_gromacs"
+  end
+
+  def caveats
+    <<~EOS
+      GMXRC and other scripts installed to:
+        #{HOMEBREW_PREFIX}/share/gromacs
+    EOS
+  end
+
+  test do
+    system "#{bin}/gmx", "help"
+  end
+end
