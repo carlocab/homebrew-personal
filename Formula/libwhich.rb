21f6f8a86042a6e3a50d0080401f6f3d59636bd6
Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
2021-07-17 16:59:31 +0000

    libwhich: add 1.1.0 bottle.

--- Formula/libwhich.rb
+++ Formula/libwhich.rb
@@ -6,6 +6,13 @@ class Libwhich < Formula
   license "MIT"
   head "https://github.com/vtjnash/libwhich.git"
 
+  bottle do
+    root_url "https://ghcr.io/v2/carlocab/personal"
+    sha256 cellar: :any_skip_relocation, big_sur:      "a43faeef4fe0d6884d7f6b83f78f1dd5c724cbac574361c689086334b0722f64"
+    sha256 cellar: :any_skip_relocation, catalina:     "279c630cfddf07d58d9c98e0208c6f02f452176585f8d30e3009b8886e32ff02"
+    sha256 cellar: :any_skip_relocation, x86_64_linux: "6633347026a69c78afc5eb57d2fb22c4f2cd01fed72b412d9b02030e1728e22c"
+  end
+
   on_linux do
     depends_on "gnu-sed" => :test
   end
