9a380426b36d3b4ff0a53230a7f202bb1869139f
Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
2024-09-04 17:50:21 +0800

    libwhich 1.2.0
    Closes #122.
    
    Signed-off-by: Carlo Cabrera <30379873+carlocab@users.noreply.github.com>

--- Formula/libwhich.rb
+++ Formula/libwhich.rb
@@ -1,8 +1,8 @@
 class Libwhich < Formula
   desc "Like `which`, for dynamic libraries"
   homepage "https://github.com/vtjnash/libwhich"
-  url "https://github.com/vtjnash/libwhich/archive/v1.1.0.tar.gz"
-  sha256 "f1c30bf7396859ad437a5db74e9e328fb4b4e1379457121e28a3524b1e3a0b3f"
+  url "https://github.com/vtjnash/libwhich/archive/refs/tags/v1.2.0.tar.gz"
+  sha256 "aa13017310f3f9b008267283c155992bb7e0f6002dafaf82e6f0dbd270c18b0a"
   license "MIT"
   head "https://github.com/vtjnash/libwhich.git"
 
@@ -17,13 +17,6 @@ class Libwhich < Formula
     depends_on "gnu-sed" => :test
   end
 
-  # Fix `test-libwhich.sh` on Linux
-  # https://github.com/vtjnash/libwhich/pull/15
-  patch do
-    url "https://github.com/vtjnash/libwhich/commit/87cffe10080c98e7b5786c5166e420bf1ada1d41.patch?full_index=1"
-    sha256 "3fde7731301750c6d1756e5b64d232ff6eebec8ed35ece3fde55f2a8fb3ca3cb"
-  end
-
   def install
     system "make"
     bin.install "libwhich"
@@ -32,14 +25,11 @@ class Libwhich < Formula
   end
 
   test do
-    on_macos do
-      assert_equal "/usr/lib/libSystem.B.dylib", shell_output("#{bin}/libwhich -p libSystem.B.dylib")
-    end
+    assert_equal "/usr/lib/libSystem.B.dylib", shell_output("#{bin}/libwhich -p libSystem.B.dylib") if OS.mac?
+    return unless OS.linux?
 
-    on_linux do
-      ENV.prepend_path "PATH", Formula["gnu-sed"].opt_libexec/"gnubin"
-      system ENV.cc, "-o", shared_library("libz"), "-shared", "-x", "c", "/dev/null"
-      system libexec/"test-libwhich.sh"
-    end
+    ENV.prepend_path "PATH", Formula["gnu-sed"].opt_libexec/"gnubin"
+    system ENV.cc, "-o", shared_library("libz"), "-shared", "-x", "c", "/dev/null"
+    system libexec/"test-libwhich.sh"
   end
 end
