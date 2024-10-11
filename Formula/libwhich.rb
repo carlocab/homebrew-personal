8a31f3df65ebb7b3dde03da37cce61635e29a755
Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
2024-10-11 19:41:35 +0000

    libwhich: update 1.2.0 bottle.

--- Formula/libwhich.rb
+++ Formula/libwhich.rb
@@ -8,12 +8,12 @@ class Libwhich < Formula
 
   bottle do
     root_url "https://ghcr.io/v2/carlocab/personal"
-    rebuild 1
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "71a5dda4fc65ded27ad4b0339b4341d562be871ffb1b2eb69ba06ea91d234ded"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "c7ea378d55b175a2197b3a1eb6f3f1deb9fa0bea1ead38ebcb9cc1591ce5e187"
-    sha256 cellar: :any_skip_relocation, ventura:       "7060045b6ee2e1aff0cad8151440cd9b26b3a6f4875b47062a0d68b401bf64aa"
-    sha256 cellar: :any_skip_relocation, monterey:      "da9995ccab645cd58c98ee8ff2954782dfaada5d5be0a668e6adbb4ff4cf5bdc"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "f7b0d2c795443617f7cba3455f02838bb28aad3295cf575bd18e5e56cfc3e566"
+    rebuild 2
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "1b49fd5b5df4d0526193b8e37d103464940430009c9af170876d945be8ea5a9c"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "e4d13ccdacedc55416224231f83a08977a1c85d35456dcb713683d45a90750e2"
+    sha256 cellar: :any_skip_relocation, ventura:       "5548500b01ed6afa0b6171e714fda2c09605fe33287afc96a7b0cb3629251359"
+    sha256 cellar: :any_skip_relocation, monterey:      "ca4d5466bf11bfe87654ce6242040680dfb799cdda623c416ceb7827fd3e2cdb"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "c3926f38987795b2359f31afbe1ba9d2955d4174f2b7aaf927e34f5fdecb39b0"
   end
 
   depends_on "gnu-sed" => :test
